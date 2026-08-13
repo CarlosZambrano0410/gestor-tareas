932b44856d1252dc0cd8a789a1c7d701143da7f8
nairb21 <bdcarranza@puce.edu.ec>
2026-08-13 00:37:46 -0500

    Mejora visualización del progreso

--- main.py
+++ main.py
@@ -11,3 +11,30 @@ def listar_tareas():
     for tarea in tareas:
         print("Tarea:", tarea["nombre"])
         print("Completada:", tarea["completada"])
+
+def mostrar_progreso():
+
+    total = len(tareas)
+ 
+    if total == 0:
+
+        print("Sin tareas")
+
+        return
+ 
+    completadas = 0
+ 
+    for tarea in tareas:
+
+        if tarea["completada"]:
+
+            completadas += 1
+ 
+    porcentaje = (
+
+        completadas * 100 / total
+
+    )
+ 
+    print("Progreso:", porcentaje, "%")
+ 
